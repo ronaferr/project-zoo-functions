0e7db1fe4f7045e0d0116daa2923f880cf7f6805
Ronaldo Carlos <ferreiraronaldocarlos@gmail.com>
2022-03-16 17:23:24 -0300

    entrega resquisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,12 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const zoneAnimal = species.find((jaula) => jaula.name === animal);
+  // filtro para pegar objeto do paramentro
+  return zoneAnimal.residents.every((resi) => resi.age >= age);
+  // retorno com teste para saber se todos atendem a condição
 }
-
+getAnimalsOlderThan();
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,8 @@
 const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids = [], ids1 = []) { //inserção de dois paramentros com default para array vazio caso não estejam vazios
+function getSpeciesByIds(ids = [], ids1 = []) {
+  // inserção de dois paramentros com default para array vazio caso não estejam vazios
   // seu código aqui
   const result = species.filter((element) => element.id === ids || element.id === ids1);
   return result;
