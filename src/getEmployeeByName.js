2904dfada2dbda975d67198286e9209ed3dd5f09
Ronaldo Carlos <ferreiraronaldocarlos@gmail.com>
2022-03-16 18:34:18 -0300

    entrega requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,16 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  // if para saber se o paramentro esta vazio, caso esteja, retornar obj vazio
+  if (!employeeName) {
+    return {};
+  }
+  // criação de variavel para filtar nome do funcionario
+  const employee = employees.filter((person) => (
+    person.firstName === employeeName || person.lastName === employeeName));
+  return employee[0];
 }
-
+console.log(getEmployeeByName());
 module.exports = getEmployeeByName;
