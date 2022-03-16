71b4ed873d0a82c22ca0967a87091852fe271a5d
Ronaldo Carlos <ferreiraronaldocarlos@gmail.com>
2022-03-16 16:48:18 -0300

    comentario em requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,10 +1,11 @@
 const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids = [], ids1 = []) {
+function getSpeciesByIds(ids = [], ids1 = []) { //inserção de dois paramentros com default para array vazio caso não estejam vazios
   // seu código aqui
   const result = species.filter((element) => element.id === ids || element.id === ids1);
   return result;
+  // criação de variavel que vai filtrar array de dados, se o elemento.id tiver o mesmo do paramentro sera retornado.
 }
 getSpeciesByIds();
 module.exports = getSpeciesByIds;
