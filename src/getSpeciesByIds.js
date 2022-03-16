89c7b207fbc1b9690b80bab2dd268e3671aa011f
Ronaldo Carlos <ferreiraronaldocarlos@gmail.com>
2022-03-16 16:45:01 -0300

    entrega requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,10 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(ids = [], ids1 = []) {
   // seu código aqui
+  const result = species.filter((element) => element.id === ids || element.id === ids1);
+  return result;
 }
-
+getSpeciesByIds();
 module.exports = getSpeciesByIds;
