a05ab48a482ab05c0687d0f3aa2665138f4fe1b6
Ronaldo Carlos <ferreiraronaldocarlos@gmail.com>
2022-03-17 16:28:55 -0300

    correçao exercicio 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,12 +1,9 @@
-const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
-  const zoneAnimal = species.find((jaula) => jaula.name === animal);
-  // filtro para pegar objeto do paramentro
-  return zoneAnimal.residents.every((resi) => resi.age >= age);
-  // retorno com teste para saber se todos atendem a condição
+  const listAnimals = data.species.filter((specie) => specie.name.includes(animal));
+  const contanier = listAnimals.map((jaula) => jaula.residents)[0].every((resi) => resi.age > age);
+  return contanier;
 }
-getAnimalsOlderThan();
 module.exports = getAnimalsOlderThan;
