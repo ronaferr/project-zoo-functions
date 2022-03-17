f7bdc13c52d5fb5129ccd60bd54c6ccfb446dfa0
Ronaldo Carlos <ferreiraronaldocarlos@gmail.com>
2022-03-17 17:03:54 -0300

    entrega requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -2,10 +2,25 @@ const data = require('../data/zoo_data');
 
 function isManager(id) {
   // seu código aqui
+  // criar função que verifica se id é gerente
+  const person = data.employees.some((objPerson) => objPerson.managers.includes(id));
+  return person;
 }
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
+  const func = isManager(managerId);
+
+  if (func === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const l = [];
+  data.employees.forEach((e) => {
+    if (e.managers.includes(managerId)) {
+      l.push(`${e.firstName} ${e.lastName}`);
+    }
+  });
+  return l;
 }
 
 module.exports = { isManager, getRelatedEmployees };
