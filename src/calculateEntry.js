59e132986b2a286380dd2fb5e5dc80bc268e5aa2
Ronaldo Carlos <ferreiraronaldocarlos@gmail.com>
2022-03-18 18:53:01 -0300

    entrega requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,36 @@
 const data = require('../data/zoo_data');
 
+const p = data.prices;
+
 function countEntrants(entrants) {
   // seu código aqui
+  const result = { child: 0, adult: 0, senior: 0 };
+  entrants.forEach((person) => {
+    if (person.age < 18) {
+      result.child += 1;
+    } else if (person.age >= 18 && person.age < 50) {
+      result.adult += 1;
+    } else {
+      result.senior += 1;
+    }
+  });
+  return result;
 }
 
 function calculateEntry(entrants) {
   // seu código aqui
+  // verificar se o parametro está vazio ou se tem um objeto vazio
+  if (!entrants || Object.values(entrants).length === 0) {
+    return 0;
+  }
+  const list = countEntrants(entrants);
+  const persons = Object.values(list);
+  const prices = Object.values(p);
+  const v = persons[0] * prices[2];
+  const v2 = persons[1] * prices[0];
+  const v3 = persons[2] * prices[1];
+  const total = v + v2 + v3;
+  return total;
 }
 
 module.exports = { calculateEntry, countEntrants };
