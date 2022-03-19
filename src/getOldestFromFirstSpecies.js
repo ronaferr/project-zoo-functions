99390b3542135180b57979dcba7bc17d2ef36d0b
Ronaldo Carlos <ferreiraronaldocarlos@gmail.com>
2022-03-19 20:48:19 -0300

    entrega requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+const s = data.species;
+const e = data.employees;
+
+function findOldAnimal(id) {
+  const findId = s.filter((specie) => specie.id === id)[0].residents;
+  const getOldAnimal = (old, actual) => ((old.age > actual.age) ? old : actual);
+  const oldAnimal = findId.reduce(getOldAnimal, 0);
+  return Object.values(oldAnimal);
 }
 
+function getOldestFromFirstSpecies(id) {
+  // seu código aqui
+  const person = e.filter((employee) => employee.id === id);
+  const animals = person.map((a) => a.responsibleFor)[0];
+  const result = findOldAnimal(animals[0]);
+  return result;
+}
 module.exports = getOldestFromFirstSpecies;
