bbc7e5904fd5ff21c0e84a7008dda023b06616e0
Ronaldo Carlos <ferreiraronaldocarlos@gmail.com>
2022-03-21 11:05:56 -0300

    entrega requisito 10

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,46 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const e = data.employees;
+const s = data.species;
+
+function dadosFuncionario(dados) {
+  const result = {};
+
+  const b = dados[0].responsibleFor;
+  const teste = b.map((ids) => (s.filter((animals) => animals.id === ids)));
+  const criaçãoDeObjetoSpecies = teste.map((animals) => animals[0].name);
+  const criaçãoDeObjetoRegiao = teste.map((animals) => animals[0].location);
+
+  const a = dados.map((valor) => ({
+    [`${'id'}`]: valor.id,
+  }));
+  a.forEach((obj1) => Object.assign(result, obj1));
+  result.fullName = `${dados[0].firstName} ${dados[0].lastName}`;
+  result.species = criaçãoDeObjetoSpecies;
+  result.locations = criaçãoDeObjetoRegiao;
+
+  // console.log(result);
+  return result;
 }
 
+function getEmployeesCoverage(obj) {
+  // seu código aqui
+  if (!obj) {
+    const every = e.map((employee) => [employee]).map((x) => dadosFuncionario(x));
+    return every;
+  }
+  // encontrar funcionário
+  const findEmployee = e.filter((person) => (
+    person.firstName.includes(obj.name)
+    || person.lastName.includes(obj.name)
+    || person.id.includes(obj.id)
+  ));
+
+  if (findEmployee.length === 0) {
+    throw new Error('Informações inválidas');
+  } else {
+    return dadosFuncionario(findEmployee);
+  }
+}
+// getEmployeesCoverage();
 module.exports = getEmployeesCoverage;
